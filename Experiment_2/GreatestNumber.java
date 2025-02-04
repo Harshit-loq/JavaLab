83daa05b138149f73e0796dee3db103d5b9ad852
Harshit-loq <harshit123952@gmail.com>
2025-02-04 12:26:43 +0530

    Finding greatest number among 3

---
+++ Experiment_2/GreatestNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class GreatestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter the third integer: ");
+        int num3 = input.nextInt();
+
+        int greatest;
+
+        if (num1 >= num2) {
+            if (num1 >= num3) {
+                greatest = num1;
+            } else {
+                greatest = num3;
+            }
+        } else {
+            if (num2 >= num3) {
+                greatest = num2;
+            } else {
+                greatest = num3;
+            }
+        }
+
+        System.out.println("The greatest number is: " + greatest);
+    }
+}
