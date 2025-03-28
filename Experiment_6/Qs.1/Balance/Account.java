259ca95952535768176cd24bf521ea0e9bffda56
Harshit-loq <harshit123952@gmail.com>
2025-03-28 05:52:51 +0530

    User Define Package

---
+++ Experiment_6/Qs.1/Balance/Account.java
@@ -0,0 +1,14 @@
+package Balance;
+
+public class Account {
+
+    private final double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void Display_Balance() {
+        System.out.println("Account Balance: $" + balance);
+    }
+}
