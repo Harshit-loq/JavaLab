05420ff919b85acd25afe6796276b0028a5716ce
Harshit-loq <harshit123952@gmail.com>
2025-03-28 05:57:56 +0530

    Create Balance package with Account class and Display_Balance method to show account balance

---
+++ Experiment_6/Qs.1/BalanceMain.java
@@ -0,0 +1,7 @@
+import Balance.Account;
+public class BalanceMain {
+    public static void main(String[] args) {
+        Account myAccount = new Account(1500.75);
+        myAccount.Display_Balance();
+    }
+}
